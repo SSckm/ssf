09f9eb98293308dd2f27e2a09082dfef947d18d1
kks <soaer_service@163.com>
2018-01-19 10:47:10 +0800

    add the template

--- nginx/lua/resty/timer/create_blog_index.lua
+++ nginx/lua/resty/timer/create_blog_index.lua
@@ -1,5 +1,27 @@
+local cache = require "common.cache"
+local constant = require "constant"
+local cjson = require "cjson"
 
-local blogListTemplate = "/home/sunny/ssf/nginx/lua/resty/template/blog_index.html"
-function read_template()
+local BLOGINDEXTEMPLATE = "/home/sunny/ssf/nginx/lua/resty/template/blog_index.html"
+
+function readTemplate()
+	local file = io.open(BLOGINDEXTEMPLATE, "r");
+    local data = file:read("*a");
+    file:close();
+    return data;
+end
+
+local blogList = cache.get_from_cache(constant.BLOG_LIST)
+if not blogList then
+	return
+end
+
+blogList = cjson.decode(blogList)
+local sources = blogList.list
+if type(sources) ~= "table" || #sources <= 0 then
+	return
+end
+
+for i,v in ipairs(sources) do
 	
 end
