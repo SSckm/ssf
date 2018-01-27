c0bd1a031fb304a89a4125a2f30d885f4e38a890
sunny <lzx@ciphergateway.com>
2018-01-27 23:50:40 +0800

    iii

--- front/operate/tool.js
+++ front/operate/tool.js
@@ -356,7 +356,7 @@ function AES_CBC_PKCS7Padding_Decrypt() {
 $(document).ready(function(){
   $("#aesEncrypt").click(function(){
       var key = $("#aesKey").val();
-      if (key == "" || key.length != 16 || key.length != 24 || key.length != 32) {
+      if (key == "" && key.length != 16 && key.length != 24 && key.length != 32) {
         alert("请输入正确长度的密钥！支持128bit/192bit/256bit")
         return;
       }
